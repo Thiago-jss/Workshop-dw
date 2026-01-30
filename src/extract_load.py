e5616d51d2adcb9d28f07dabd722bed6ea9fae87
Thiago-jss <thiagojss11@gmail.com>
2026-01-30 14:46:41 -0300

    feat: New extract and load features added

--- src/extract_load.py
+++ src/extract_load.py
@@ -1,13 +1,27 @@
 import yfinance as yf
 import pandas as pd
-import dotenv
+from sqlalchemy import create_engine
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 
 commodities = ["GC=F","SI=F","CL=F",]
 
+
+
+DB_HOST= os.getenv("DB_HOST")
+DB_PORT= os.getenv("DB_PORT")
+DB_NAME= os.getenv("DB_NAME")
+DB_USER= os.getenv("DB_USER")
+DB_PASSWORD= os.getenv("DB_PASSWORD")
+DB_SCHEMA=os.getenv("DB_SCHEMA")
+
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+engine = create_engine(DATABASE_URL)
+
 def search_commodity_data(symbol, period="5d", interval="1d"):
-    ticker = yf.Ticker('CL=F')
+    ticker = yf.Ticker(symbol)
     data = ticker.history(period=period, interval=interval)[['Close']]
     data['Symbol'] = symbol
     return data
@@ -19,7 +33,11 @@ def fetch_all_commodities_data(commodities):
         all_data.append(data)
     return pd.concat(all_data)
 
+def save_to_database(df, schema='public' ):
+    df.to_sql('commodities', engine,if_exists='replace', index=True, index_label='Date', schema=schema)
+
+
 if __name__ == "__main__":
     concatenated_data = fetch_all_commodities_data(commodities)
-    print(concatenated_data)
+    save_to_database(concatenated_data, schema='public')
 
